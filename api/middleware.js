afab792750de422d52ba47cb7c2bdf004d5ea831
Ben Cutler <bcutler94@gmail.com>
2020-09-22 15:40:11 -0400

    added some middleware logging

---
+++ api/middleware.js
@@ -0,0 +1,13 @@
+class MiddleWare {
+    static TimeEndpoint = (req, res, next) => {
+        console.time('Endpoint time');
+        res.on("finish", () => {
+            console.timeEnd('Endpoint time');
+        });
+        next()
+    }
+}
+
+module.exports = {
+    MiddleWare
+}
--- index.js
+++ index.js
@@ -5,8 +5,10 @@ const PORT = process.env.PORT || 8000;
 const { 
     Endpoints: { FindLuggage } 
 } = require('./api/endpoints');
+const { MiddleWare } = require('./api/middleware');
 
 app.use(json());
+app.use(MiddleWare.TimeEndpoint)
 
 app.post('/findLuggage', FindLuggage);
 
